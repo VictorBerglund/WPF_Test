cac6a8a36337e90b3a470e89a7c10d8016d5cbfd
Victor Berglund <vicberg1@outlook.com>
2018-11-21 20:35:10 +0100

    Some MVVM stuff with Caliburn.Micro

---
+++ WPF_Test/WPF_Test/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF_Test.ViewModels;
+
+namespace WPF_Test
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+		public Bootstrapper()
+		{
+			Initialize();
+		}
+
+		protected override void OnStartup(object sender, StartupEventArgs e)
+		{
+			DisplayRootViewFor<ShellViewModel>();
+		}
+	}
+}
